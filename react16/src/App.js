6e09b65fba2b69e3c8f2d7aa31a008c5b96db3f0
朱航 <zhuhang@stu.xmu.edu.cn>
2022-09-13 11:13:03 +0800

    feat: 新增react16子应用

---
+++ react16/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './components/Counter.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>当前为react16应用</h1>
+        <Counter></Counter>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
